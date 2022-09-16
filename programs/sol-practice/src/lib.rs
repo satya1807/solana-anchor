7405545b6f9be5b6054075f30cfba71649a3cafd
satya1807 <satyanalam9@gmail.com>
2022-09-16 17:34:50 +0530

    added: only owner access

--- programs/sol-practice/src/lib.rs
+++ programs/sol-practice/src/lib.rs
@@ -1,14 +1,14 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Hy7s8s286KFx1fQwVuBntSxsWp7Cr8EAtMcoG4y7VZ7N");
+declare_id!("6vFvKHXtV5jdJDTY2AcBeyi4sXJ3LhVoydd6i2Pszz1T");
 
 #[program]
 pub mod counter {
     use super::*;
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
+        counter.user = ctx.accounts.user.key();
         Ok(())
     }
 
@@ -21,6 +21,7 @@ pub mod counter {
     pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
         counter.count -= 1;
+        msg!("Owner: {}", counter.user);
         Ok(())
     }
 
@@ -34,37 +35,35 @@ pub mod counter {
 #[account]
 pub struct Counter {
     count: i32,
+    user: Pubkey,
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer=signer, space=32)]
+    #[account(init, payer=user, space=120)]
     pub counter: Account<'info, Counter>,
     #[account(mut)]
-    pub signer: Signer<'info>,
+    pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct Increment<'info> {
-    #[account(mut)]
+    #[account(mut, has_one=user)]
     pub counter: Account<'info, Counter>,
-    #[account(mut)]
-    pub caller: Signer<'info>,
+    pub user: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct Decrement<'info> {
-    #[account(mut)]
+    #[account(mut, has_one=user)]
     pub counter: Account<'info, Counter>,
-    #[account(mut)]
-    pub caller: Signer<'info>,
+    pub user: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct Reset<'info> {
-    #[account(mut)]
+    #[account(mut, has_one=user)]
     pub counter: Account<'info, Counter>,
-    #[account(mut)]
-    pub caller: Signer<'info>,
+    pub user: Signer<'info>,
 }
--- tests/Counter.test.js
+++ tests/Counter.test.js
@@ -2,102 +2,107 @@ const anchor = require("@project-serum/anchor");
 const { expect } = require("chai");
 
 describe("sol-practice", async () => {
-  let counterAccount;
+	let counterAccount;
 
-  const provider = anchor.AnchorProvider.local();
-  anchor.setProvider(provider);
+	const provider = anchor.AnchorProvider.local();
+	anchor.setProvider(provider);
 
-  const program = anchor.workspace.Counter;
+	const program = anchor.workspace.Counter;
 
-  beforeEach(async () => {
-    counterAccount = anchor.web3.Keypair.generate();
-    await program.methods
-      .initialize()
-      .accounts({
-        counter: counterAccount.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .signers([counterAccount])
-      .rpc();
-  });
+	beforeEach(async () => {
+		counterAccount = anchor.web3.Keypair.generate();
+		await program.methods
+			.initialize()
+			.accounts({
+				counter: counterAccount.publicKey,
+				user: provider.wallet.publicKey,
+				systemProgram: anchor.web3.SystemProgram.programId,
+			})
+			.signers([counterAccount])
+			.rpc();
+	});
 
-  it("..should verify the deployment value", async () => {
-    const counter = await program.account.counter.fetch(
-      counterAccount.publicKey
-    );
-    expect(counter.count).to.equal(0);
-  });
+	it(".. should verify the deployment value", async () => {
+		const counter = await program.account.counter.fetch(
+			counterAccount.publicKey
+		);
+		expect(counter.count).to.equal(0);
+		expect(counter.user.toString()).to.equal(
+			provider.wallet.publicKey.toString()
+		);
+	});
 
-  it(".. should test increment function", async () => {
-    await program.methods
-      .increment()
-      .accounts({
-        counter: counterAccount.publicKey,
-        caller: provider.wallet.publicKey,
-      })
-      .rpc();
+	it(".. should test increment function", async () => {
+		await program.methods
+			.increment()
+			.accounts({
+				counter: counterAccount.publicKey,
+				user: provider.wallet.publicKey,
+			})
+			.rpc();
 
-    const counter = await program.account.counter.fetch(
-      counterAccount.publicKey
-    );
-    expect(counter.count).to.equal(1);
-  });
+		const counter = await program.account.counter.fetch(
+			counterAccount.publicKey
+		);
+		expect(counter.count).to.equal(1);
+	});
 
-  it(".. should test decrement function", async () => {
-    await program.methods
-      .decrement()
-      .accounts({
-        counter: counterAccount.publicKey,
-        caller: provider.wallet.publicKey,
-      })
-      .rpc();
+	it(".. should test decrement function", async () => {
+		await program.methods
+			.decrement()
+			.accounts({
+				counter: counterAccount.publicKey,
+				user: provider.wallet.publicKey,
+			})
+			.rpc();
 
-    const counter = await program.account.counter.fetch(
-      counterAccount.publicKey
-    );
+		const counter = await program.account.counter.fetch(
+			counterAccount.publicKey
+		);
 
-    expect(counter.count).to.equal(-1);
-  });
+		expect(counter.count).to.equal(-1);
+	});
 
-  it(".. should test reset function", async () => {
-    await program.methods
-      .increment()
-      .accounts({
-        counter: counterAccount.publicKey,
-        caller: provider.wallet.publicKey,
-      })
-      .rpc();
+	it(".. should test reset function", async () => {
+		await program.methods
+			.increment()
+			.accounts({
+				counter: counterAccount.publicKey,
+				user: provider.wallet.publicKey,
+			})
+			.rpc();
 
-    await program.methods
-      .increment()
-      .accounts({
-        counter: counterAccount.publicKey,
-        caller: provider.wallet.publicKey,
-      })
-      .rpc();
+		await program.methods
+			.increment()
+			.accounts({
+				counter: counterAccount.publicKey,
+				user: provider.wallet.publicKey,
+			})
+			.rpc();
 
-    await program.methods
-      .increment()
-      .accounts({
-        counter: counterAccount.publicKey,
-        caller: provider.wallet.publicKey,
-      })
-      .rpc();
+		await program.methods
+			.increment()
+			.accounts({
+				counter: counterAccount.publicKey,
+				user: provider.wallet.publicKey,
+			})
+			.rpc();
 
-    let counter = await program.account.counter.fetch(counterAccount.publicKey);
-    expect(counter.count).to.equal(3);
+		let counter = await program.account.counter.fetch(
+			counterAccount.publicKey
+		);
+		expect(counter.count).to.equal(3);
 
-    // incremented 3 times and now resetting
-    await program.methods
-      .reset()
-      .accounts({
-        counter: counterAccount.publicKey,
-        caller: provider.wallet.publicKey,
-      })
-      .rpc();
+		// incremented 3 times and now resetting
+		await program.methods
+			.reset()
+			.accounts({
+				counter: counterAccount.publicKey,
+				user: provider.wallet.publicKey,
+			})
+			.rpc();
 
-    counter = await program.account.counter.fetch(counterAccount.publicKey);
-    expect(counter.count).to.equal(0);
-  });
+		counter = await program.account.counter.fetch(counterAccount.publicKey);
+		expect(counter.count).to.equal(0);
+	});
 });
